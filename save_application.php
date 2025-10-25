674664c00dec33b4bbe3997879af6cdc0204d316
MAC <mac@MACs-MacBook-Pro-2.local>
2025-10-26 00:32:54 +0100

    minor bug fixes

--- save_application.php
+++ save_application.php
@@ -18,6 +18,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $passport_photo = basename($_FILES['passport_photo']['name']);
     $payment_receipt = basename($_FILES['payment_receipt']['name']);
 
+    if (!is_dir('uploads/')) mkdir('uploads/', 0777, true);
+
     move_uploaded_file($_FILES['birth_certificate']['tmp_name'], "uploads/" . $birth_certificate);
     move_uploaded_file($_FILES['lga_certificate']['tmp_name'], "uploads/" . $lga_certificate);
     move_uploaded_file($_FILES['passport_photo']['tmp_name'], "uploads/" . $passport_photo);
